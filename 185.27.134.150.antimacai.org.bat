772def8042f5b80965377e1685ec2314f33cc462
TerlajakPopular <putradany77@gmail.com>
2021-04-18 12:01:23 +0800

    Curl Script

---
+++ 185.27.134.150.antimacai.org.bat
@@ -0,0 +1,6 @@
+rem 185.27.134.150.antimacai.org
+@cls
+cd %~DP0
+@echo Updating FreeDNS
+@C:\CURL\CURL -k https://freedns.afraid.org/dynamic/update.php?RXczRmxDVjh6Ynd5MHRiNGtzUmF5Z0hFOjE5NzAyNDI2
+@pause

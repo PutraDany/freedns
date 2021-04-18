902f0196cb4050ad939b1ec22efd571fe24416e9
TerlajakPopular <putradany77@gmail.com>
2021-04-18 12:09:07 +0800

    Curl Script

---
+++ LilyRader96.antimacai.org.bat
@@ -0,0 +1,6 @@
+rem LilyRader96.antimacai.org
+@cls
+cd %~DP0
+@echo Updating FreeDNS
+@C:\CURL\CURL -k https://freedns.afraid.org/dynamic/update.php?RXczRmxDVjh6Ynd5MHRiNGtzUmF5Z0hFOjE5NzAyMDg0
+@pause
